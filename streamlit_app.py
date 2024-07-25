dbc4c5e35a49f65ebb08cb049ce90ec671a3c8ad
leoboy14 <lfortin.work@gmail.com>
2024-07-25 04:46:46 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,9 @@
 import streamlit as st
 
-col1, col2 = st.columns(2)
 
-with col1:
-    st.header("Picture")
-
-with col2:
-
-    st.markdown("<h1 style='margin-bottom: -35px;'>Leonhel V. Fortin</h1>", unsafe_allow_html=True)
-    st.markdown("<h3 class='title-header'margin-bottom: -10px; style='color: #60b4ff;'>Artificial Intelligence Engineer</h3>", unsafe_allow_html=True)
-    st.write("Leonhel V. Fortin is an accomplished Artificial Intelligence Engineer specializing in developing and implementing AI solutions. Leonhel is passionate about leveraging technology for smart city initiatives and fostering creative industry growth.", unsafe_allow_html=True)
+st.markdown("<h1 style='margin-bottom: -35px;'>Leonhel V. Fortin</h1>", unsafe_allow_html=True)
+st.markdown("<h3 class='title-header'margin-bottom: -10px; style='color: #60b4ff;'>Artificial Intelligence Engineer</h3>", unsafe_allow_html=True)
+st.write("Leonhel V. Fortin is an accomplished Artificial Intelligence Engineer specializing in developing and implementing AI solutions. Leonhel is passionate about leveraging technology for smart city initiatives and fostering creative industry growth.", unsafe_allow_html=True)
 
 with st.container():
    st.write("This is inside the container")
